6c4b2fb6de6c599f4cb55ce4dc988c8d0189ffd3
KyahWel <williamcris.hod02@gmail.com>
2024-03-16 22:37:39 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@ant-design-vue/nuxt',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+  ],
+
+ // buildModules: ['@nuxt/typescript-build'], 
+  css: ['~/assets/css/main.css','~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  antd:{
+    // Options
+  }
+})
